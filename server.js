c6d4ac73414ed6e345633cd7f9b699eb1a9109ca
swapnilgupta14 <swapnilguptaswag@gmail.com>
2025-01-04 12:41:36 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+import { create, router as _router, defaults } from "json-server";
+const server = create();
+const router = _router("db.json");
+const middlewares = defaults();
 
 server.use(middlewares);
 server.use(router);
