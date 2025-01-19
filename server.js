8befc4d7be36d37e8bdcda27d7d349555654c71b
swapnilgupta14 <swapnilguptaswag@gmail.com>
2025-01-19 12:06:14 +0530

    Endpoint Added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
+const fs = require("fs");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
@@ -8,6 +9,12 @@ const port = process.env.PORT || 3000;
 
 server.use(cors());
 server.use(middlewares);
+
+server.get("/", (req, res) => {
+  const db = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+  res.json(db);
+});
+
 server.use(router);
 
 server.listen(port, () => {
